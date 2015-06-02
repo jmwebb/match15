7ea66bc079fd8363e670d64440b06461d7b1b13e
Santiago Seira <sseira@stanford.edu>
2015-06-02 11:05:50 -0700

    all names finally included, commented out unnecessary formatting

--- app/importNames/importNames.js
+++ app/importNames/importNames.js
@@ -7,38 +7,8 @@ angular.module('myApp').factory('importNames', function($http) {
         get: function() {
             if ( !promise ) {
                 return $http.get('importNames/names_emails.json');
-                //  .success(function(response) {
-                //    var name_emailJSON = response;
-                //    var names_emails = [];
-                //    name_emailJSON.forEach(function(name_email) {
-                //      var name = name_email.current_name_sort; // names come last, first, middle?
-                //      var email = name_email.pref_email;
-                //      var name_array = name.split(',');
-                //
-                //      if (name_array.length == 3) {   // has a middle name
-                //        var first_name = name_array[1];
-                //
-                //        name_array[1] = name_array[2]         //  middle name to the middle
-                //        name_array[2] = name_array[0];        //  last names moves last
-                //        name_array[0] = first_name;
-                //
-                //      } else {     // no middle name
-                //        var first_name = name_array[1];
-                //        name_array[1] = name_array[0];
-                //        name_array[0] = first_name;
-                //      }
-                //      name = name_array.join(' ');
-                //
-                //      var name_emailObj = {};
-                //      name_emailObj.name = name;
-                //      name_emailObj.email = email;
-                //      names_emails.push(name_emailObj);
-                //    });
-                //    return names_emails; //names_emails;
-                //  });
-                //return promise; //promise;
             }
         }
     };
     return jsondata;
-})
+});
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,17 +1,11 @@
 'use strict';
 
 angular.module('myApp')
-//    .view1', ['ngRoute', 'firebase'])
-//
 
-
-.controller('View1Ctrl', function($scope, $http, $firebaseObject, $firebaseAuth, importNames, FIREBASE_URL, $modal) {
+.controller('View1Ctrl', function($scope, $http, $firebaseObject, importNames, FIREBASE_URL, $modal) {
 
     $scope.showRules = true;
-    $scope.choicesLust = ['choice0', 'choice1', 'choice2', 'choice3', 'choice4', 'choice5', 'choice6', 'choice7', 'choice8', 'choice9'];
-
-    $scope.choicesFriends = [ 'choice10', 'choice11', 'choice12', 'choice13', 'choice14'];
-
+    $scope.choices = ['choice0', 'choice1', 'choice2', 'choice3', 'choice4', 'choice5', 'choice6', 'choice7', 'choice8', 'choice9', 'choice10', 'choice11', 'choice12', 'choice13', 'choice14'];
 
 
 
@@ -32,9 +26,9 @@ angular.module('myApp')
         $scope.firstName =  $scope.userName.split(' ')[0];
         // split name into firstmMiddleLast
         // eliminate any . .", "#", "$", "[", or "]".git
-        var user_ref = new Firebase(FIREBASE_URL + '/' + $scope.userName.replace(/\W/g, ''));
+        //var user_ref = new Firebase(FIREBASE_URL + '/' + $scope.userName.replace(/\W/g, ''));
 
-        //var user_ref = new Firebase(FIREBASE_URL + '/sdfasd');        // for development
+        var user_ref = new Firebase(FIREBASE_URL + '/sdfawesd');        // for development
 
         $scope.user = $firebaseObject(user_ref);
 
@@ -97,51 +91,135 @@ angular.module('myApp')
 
 // -----------------  typeahead methods ----------------------------- //
   importNames.get().success(function(response) {
+
     var name_emailJSON = response;
     var names_emails = [];
-    var suid_array = [];
-    var suids = "";
-    var namesWithoutSUIDS = [];
+    //$scope.suid_array = [];
+    //var suids = "";
+    //var namesWithoutSUIDS = [];
+
+
+      //function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
+      //    //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+      //    var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+      //
+      //    var CSV = '';
+      //    //Set Report title in first row or line
+      //
+      //    CSV += ReportTitle + '\r\n\n';
+      //
+      //    //This condition will generate the Label/Header
+      //    if (ShowLabel) {
+      //        var row = "";
+      //
+      //        //This loop will extract the label from 1st index of on array
+      //        for (var index in arrData[0]) {
+      //
+      //            //Now convert each value to string and comma-seprated
+      //            row += index + ',';
+      //        }
+      //
+      //        row = row.slice(0, -1);
+      //
+      //        //append Label row with line break
+      //        CSV += row + '\r\n';
+      //    }
+      //
+      //    //1st loop is to extract each row
+      //    for (var i = 0; i < arrData.length; i++) {
+      //        var row = "";
+      //
+      //        //2nd loop will extract each column and convert it in string comma-seprated
+      //        for (var index in arrData[i]) {
+      //            row += '"' + arrData[i][index] + '",';
+      //        }
+      //
+      //        row.slice(0, row.length - 1);
+      //
+      //        //add a line break after each row
+      //        CSV += row + '\r\n';
+      //    }
+      //
+      //    if (CSV == '') {
+      //        alert("Invalid data");
+      //        return;
+      //    }
+      //
+      //    //Generate a file name
+      //    var fileName = "MyReport_";
+      //    //this will remove the blank-spaces from the title and replace it with an underscore
+      //    fileName += ReportTitle.replace(/ /g,"_");
+      //
+      //    //Initialize file format you want csv or xls
+      //    var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+      //
+      //    // Now the little tricky part.
+      //    // you can use either>> window.open(uri);
+      //    // but this will not work in some browsers
+      //    // or you will not get the correct file extension
+      //
+      //    //this trick will generate a temp <a /> tag
+      //    var link = document.createElement("a");
+      //    link.href = uri;
+      //
+      //    //set the visibility hidden so it will not effect on your web-layout
+      //    link.style = "visibility:hidden";
+      //    link.download = fileName + ".csv";
+      //
+      //    //this part will append the anchor tag and remove it after automatic click
+      //    document.body.appendChild(link);
+      //    link.click();
+      //    document.body.removeChild(link);
+      //}
+
+
+
     name_emailJSON.forEach(function(name_email) {
-      var name = name_email.current_name_sort; // names come last, first, middle?
-      var email = name_email.pref_email;
-      var name_array = name.split(',');
+      //var name = name_email.name; // names come last, first, middle?
+      //var email = name_email.email;
+      //
+      ////var name_array = name.split(',');
+      ////
+      ////if (name_array.length == 3) {   // has a middle name
+      ////  var first_name = name_array[1];
+      ////
+      ////  name_array[1] = name_array[2]         //  middle name to the middle
+      ////  name_array[2] = name_array[0];        //  last names moves last
+      ////  name_array[0] = first_name;
+      ////
+      ////} else {     // no middle name
+      ////  var first_name = name_array[1];
+      ////  name_array[1] = name_array[0];
+      ////  name_array[0] = first_name;
+      ////}
+      ////name = name_array.join(' ');
+      ////
+      ////name = $scope.capitalizeEveryWord(name);
+      //
+      //var name_emailObj = {};
+      //name_emailObj.name = name;
+      //name_emailObj.email = email;
+      names_emails.push(name_email);
 
-      if (name_array.length == 3) {   // has a middle name
-        var first_name = name_array[1];
 
-        name_array[1] = name_array[2]         //  middle name to the middle
-        name_array[2] = name_array[0];        //  last names moves last
-        name_array[0] = first_name;
 
-      } else {     // no middle name
-        var first_name = name_array[1];
-        name_array[1] = name_array[0];
-        name_array[0] = first_name;
-      }
-      name = name_array.join(' ');
+      // eventually also add the new 171 missing SUIDs
 
-      name = $scope.capitalizeEveryWord(name);
 
-      // -----------------  emails to SUIDs separate into comma separated values-------------------------//
 
-      var email_domain = email.split('@')[1];
+        // -----------------  emails to SUIDs separate into comma separated values -------------------------//  to delete everything below
 
-      if (email_domain == 'stanford.edu') {
-        suid_array.push(email.split('@')[0]);
-      } else {
-        namesWithoutSUIDS.push(name);
-      }
+        //var email_domain = email.split('@')[1];
+        //
+        //if (email_domain == 'stanford.edu') {
+        //    $scope.suid_array.push(email.split('@')[0]);
+        //} else {
+        //    namesWithoutSUIDS.push(name);
+        //}
 
-      var name_emailObj = {};
-      name_emailObj.name = name;
-      name_emailObj.email = email;
-      names_emails.push(name_emailObj);
     });
-
-    suids = suid_array.join(',');
-    var noSUID = namesWithoutSUIDS.join(', ');
     $scope.namesEmails = names_emails;
+        //console.log($scope.namesEmails.length);
     $scope.loadUser();
   });
 
